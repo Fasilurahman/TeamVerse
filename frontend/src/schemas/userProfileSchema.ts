ae53df6359c5008d6ba65944340e411cfcf6dd66
Fasilurahman <131866940+Fasilurahman@users.noreply.github.com>
2025-08-14 10:04:49 +0530

    Update userProfileSchema.ts

--- frontend/src/schemas/userProfileSchema.ts
+++ frontend/src/schemas/userProfileSchema.ts
@@ -1,11 +1,18 @@
 import { z } from "zod"
 
 export const userProfileSchema = z.object({
-  name: z.string().min(2, { message: "First name must be at least 2 characters long" }),
-  email: z.string().email({ message: "Invalid email address" }),
+  name: z.string()
+    .min(2, { message: "First name must be at least 2 characters long" })
+    .max(20, { message: "First name must be at most 50 characters long" }),
+    
+  email: z.string()
+    .email({ message: "Invalid email address" }),
+    
   location: z.enum(["Ireland", "USA", "UK"], { message: "Invalid location" }),
+  
   phone: z.string()
-    .optional() 
+    .max(15, { message: "Phone number must be at most 15 digits long" }) 
+    .optional()
     .refine((val) => !val || /^\+?[1-9]\d{1,14}$/.test(val), {
       message: "Invalid phone number",
     })
